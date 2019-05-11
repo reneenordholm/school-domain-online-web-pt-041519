4acf96e2ed6469304471d39a48db08710a0a865c
Renee Nordholm <reneenordholm@gmail.com>
2019-05-11 19:20:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,9 +11,9 @@ class School
   def add_student(name, grade)
     roster[grade] = []
     if roster.include?(grade)
-      roster[grade] = 
+      roster[grade] = name
     else 
-
+      roster[grade] << name
     end
   end
 end

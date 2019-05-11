fbc8169a3002c5570519cbdcb079d69cac47df41
Renee Nordholm <reneenordholm@gmail.com>
2019-05-11 18:24:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,13 @@
 class School
-  attr_accessor(:name)
+  attr_accessor(:name, :roster)
   
   roster = {}
   
   def initialize(name)
     @name = name
-    
+  end
+  
+  def roster(roster)
+    @roster = roster
   end
 end
